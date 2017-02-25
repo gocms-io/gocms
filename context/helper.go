017a5f7c69f9c9bd05f0469c583fb57201110f3d
Justin Menkveld <justin@menklab.com>
2017-02-25 14:30:23 -0500

    updated to actual print string on fail

--- context/helper.go
+++ context/helper.go
@@ -20,7 +20,7 @@ func GetIntOrFail(s string, settings map[string]goCMS_models.Setting) int64 {
 	is := settings[s].Value
 	i, err := strconv.ParseInt(is, 10, 34)
 	if err != nil {
-		fmt.Println("Error parsing string: " + is + " into int: " + err.Error())
+		fmt.Println("Error parsing string: " + s + " into int: " + err.Error())
 		os.Exit(0)
 	}
 	return i
@@ -28,18 +28,18 @@ func GetIntOrFail(s string, settings map[string]goCMS_models.Setting) int64 {
 
 func GetStringOrFail(s string, settings map[string]goCMS_models.Setting) string {
 
-	s = settings[s].Value
-	if s == "" {
+	ss := settings[s].Value
+	if ss == "" {
 		fmt.Println("Error retrieving string: " + s)
 		os.Exit(1)
 	}
-	return s
+	return ss
 }
 func GetBoolOrFail(s string, settings map[string]goCMS_models.Setting) bool {
 	bs := settings[s].Value
 	b, err := strconv.ParseBool(bs)
 	if err != nil {
-		fmt.Println("Error parsing bool: " + bs + " into bool: " + err.Error())
+		fmt.Println("Error parsing bool: " + s + " into bool: " + err.Error())
 		os.Exit(0)
 	}
 	return b
