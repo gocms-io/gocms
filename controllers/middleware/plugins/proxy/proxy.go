7ba25456c7a818ffd624e67d8798e0239b86532a
Justin Menkveld <justin@menklab.com>
2017-03-03 15:29:00 -0500

    added sending auth user info through proxy

--- controllers/middleware/plugins/proxy/proxy.go
+++ controllers/middleware/plugins/proxy/proxy.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"github.com/menklab/goCMS/utility/errors"
 	"net/http"
+	"github.com/menklab/goCMS/utility"
+	"strconv"
 )
 
 type PluginProxyMiddleware struct {
@@ -21,6 +23,12 @@ func (ppm *PluginProxyMiddleware) ReverseProxy() gin.HandlerFunc {
 }
 
 func (ppm *PluginProxyMiddleware) reverseProxy(c *gin.Context) {
+
+	// check to see if authUser available
+	authUser, _ := utility.GetUserFromContext(c)
+	c.Request.Header.Set("GOCMS-AUTH-USER-ID", strconv.Itoa(authUser.Id))
+	c.Request.Header.Set("GOCMS-AUTH-NAME", authUser.FullName)
+	c.Request.Header.Set("GOCMS-AUTH-EMAIL", authUser.Email)
 
 	url, err := url.Parse(fmt.Sprintf("%s://%s:%d", ppm.Schema, ppm.Host, ppm.Port))
 	if err != nil {
