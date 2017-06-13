b08555074521bc5f6e98703b62072a2c63520662
Justin Menkveld <justin@menklab.com>
2017-06-13 01:20:22 -0400

    bump

---
+++ build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# for testing. comment out for release.
+TRAVIS_BRANCH="dev"
+
+# grab govendor and sync
+echo install govendor
+go get -u github.com/kardianos/govendor
+
+echo pulling in deps with govendor
+govendor sync
+
+
+
+# build linux64
+echo build linux64
+GOOS=linux GOARCH=amd64 go build -o bin/$TRAVIS_BRANCH/linux_64/gocms-services
+pushd bin/$TRAVIS_BRANCH/linux_64
+zip gocms-services.zip gocms-services
+rm gocms-services
+popd
+
+#build linux32
+echo build linux32
+GOOS=linux GOARCH=386 go build -o bin/$TRAVIS_BRANCH/linux_32/gocms-services
+pushd bin/$TRAVIS_BRANCH/linux_32
+zip gocms-services.zip gocms-services
+rm gocms-services
+popd
+
+#build arm
+echo build linux_arm
+GOOS=linux GOARCH=arm go build -o bin/$TRAVIS_BRANCH/linux_arm/gocms-services
+pushd bin/$TRAVIS_BRANCH/linux_arm
+zip gocms-services.zip gocms-services
+rm gocms-services
+popd
+
+#build osx
+echo build osx
+GOOS=darwin GOARCH=amd64 go build -o bin/$TRAVIS_BRANCH/osx/gocms-services
+pushd bin/$TRAVIS_BRANCH/osx
+zip gocms-services.zip gocms-services
+rm gocms-services
+popd
+
+#build win64
+echo build win64
+GOOS=windows GOARCH=amd64 go build -o bin/$TRAVIS_BRANCH/windows_64/gocms-services.exe
+pushd bin/$TRAVIS_BRANCH/windows_64
+zip gocms-services.zip gocms-services.exe
+rm gocms-services.exe
+popd
+
+#build win32
+echo build win32
+GOOS=windows GOARCH=386 go build -o bin/$TRAVIS_BRANCH/windows_32/gocms-services.exe
+pushd bin/$TRAVIS_BRANCH/windows_32
+zip gocms-services.zip gocms-services.exe
+rm gocms-services.exe
+popd
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# vars for testing. comment out for release.
+AWS_SECRET="XiILbK7edn2s7tmN2CuNzhw9NwYU5Yt6r1bdtB5d"
+AWS_BUCKET="release.gocms.io"
+AWS_KEY="AKIAJJOXWVHFZZEJIQRQ"
+TRAVIS_BRANCH="dev"
+TRAVIS_BUILD_NUMBER="1"
+TRAVIS_COMMIT="lkjdaldsfasa"
+
+# copy files to s3
+echo copy files to current release dir
+AWS_ACCESS_KEY_ID=$AWS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET aws s3 cp \
+    --recursive bin/$TRAVIS_BRANCH/ s3://release.gocms.io/$TRAVIS_BRANCH/current/
+
+# copy files to build number bucket
+echo copy files to build # release dir
+AWS_ACCESS_KEY_ID=$AWS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET aws s3 cp \
+    --recursive s3://release.gocms.io/$TRAVIS_BRANCH/current/ s3://release.gocms.io/$TRAVIS_BRANCH/$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT/
