7d8c8637da57be9db6713c302a9d99c5ee063231
Justin Menkveld <justin@menklab.com>
2017-06-15 05:38:09 -0400

    update deploy to do some version stuff

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GOCMS_VERSION=0.0.$TRAVIS_BUILD_NUMBER
+
 # vars for testing. comment out for release.
 #AWS_SECRET=
 #AWS_BUCKET=
@@ -8,12 +10,14 @@
 #TRAVIS_BUILD_NUMBER=
 #TRAVIS_COMMIT=
 
+# add commit tag
+echo $TRAVIS_COMMIT > bin/$TRAVIS_BRANCH/$GOCMS_VERSION
+
 # copy files to s3
 echo copy files to current release dir
 AWS_ACCESS_KEY_ID=$AWS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET aws s3 cp \
     --recursive bin/$TRAVIS_BRANCH/ s3://release.gocms.io/$TRAVIS_BRANCH/current/
 
 # copy files to build number bucket
-echo copy files to build # release dir
 AWS_ACCESS_KEY_ID=$AWS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET aws s3 cp \
-    --recursive s3://release.gocms.io/$TRAVIS_BRANCH/current/ s3://release.gocms.io/$TRAVIS_BRANCH/$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT/
+    --recursive s3://release.gocms.io/$TRAVIS_BRANCH/current/ s3://release.gocms.io/$TRAVIS_BRANCH/$GOCMS_VERSION/
