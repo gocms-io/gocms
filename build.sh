7a1e00710e4ad7f7be0c2e708ca04909cad2f9e5
Justin Menkveld <justin@menklab.com>
2017-06-15 06:27:24 -0400

    bump

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo pulling in deps with govendor
 govendor sync
 
 function buildArch() {
-    GOOS=$1 GOARCH=$2 go build -o bin/$TRAVIS_BRANCH/$3/gocms
+    GOOS=$1 GOARCH=$2 go build -o bin/$TRAVIS_BRANCH/$3/$4
     cp -r content bin/$TRAVIS_BRANCH/$3/.
     cp .env_default bin/$TRAVIS_BRANCH/$3/.env
     pushd bin/$TRAVIS_BRANCH/$3
@@ -22,9 +22,9 @@ function buildArch() {
 }
 
 # build linux64
-buildArch linux amd64 linux_64
-buildArch linux 386 linux_32
-buildArch linux arm linux_arm
-buildArch darwin amd64 osx_64
-buildArch windows amd64 windows_64
-buildArch windows 386 windows_32
+buildArch linux amd64 linux_64 gocms
+buildArch linux 386 linux_32 gocms
+buildArch linux arm linux_arm gocms
+buildArch darwin amd64 osx_64 gocms
+buildArch windows amd64 windows_64 gocms.exe
+buildArch windows 386 windows_32 gocms.exe
