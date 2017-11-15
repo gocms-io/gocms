8361cfb001aad343228cf7680cba1162045016ba
Justin Menkveld <justin@menklab.com>
2017-11-15 01:59:39 -0500

    - adding documentation - working on remove “admin” route and adding in permissions for the plugins to subscribe too.

--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,16 @@ package routes
 
 import "github.com/gin-gonic/gin"
 
+// Routes are the most basic organizational option available. They define urls as well as what level authentication is required.
 type Routes struct {
-	Public       *gin.RouterGroup
+	// Root provides access to / and allows for just about anything. No authentication is applied.
+	Root *gin.RouterGroup
+	// Public provides access to the /api route. No authentication is applied.
+	Public *gin.RouterGroup
+	// PreTwoFactor provides access to the /api route. It requires user authenticate through stage 1 (ie. Username/Password), but doesn't require stage 2 authentication. (TwoFactor Must Be Active)
 	PreTwofactor *gin.RouterGroup
-	Auth         *gin.RouterGroup
-	// todo remove admin and replace with permissions and acl layer
-	Admin   *gin.RouterGroup
-	Root    *gin.RouterGroup
+	// Auth requires that the user is fully authenticated. This could be just stage 1 or both stage 1 and 2 depending on if two factor is disabled or enabled respectively.
+	Auth *gin.RouterGroup
+	// NoRoute is not currently available to plugins.
 	NoRoute func(...gin.HandlerFunc)
 }
